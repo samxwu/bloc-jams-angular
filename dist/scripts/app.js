0841b7b9611d94a8304d54d8939d6333eafaa715
Sam Wu <sam.x.wu@gmail.com>
2016-11-14 12:06:27 -0500

    Directives-Pt1: Completed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -5,24 +5,26 @@
                 enabled: true,
                 requireBase: false
             });
-     $stateProvider
-         .state('landing', {
-             url: '/',
-             controller: 'LandingCtrl as landing',
-             templateUrl: '/templates/landing.html'
-         })
-         .state('album', {
-             url: '/album',
-             controller: 'AlbumCtrl as album',
-             templateUrl: '/templates/album.html'
-         })
-        .state('collection', {
-             url: '/collection',
-             controller: 'CollectionCtrl as collection',
-             templateUrl: '/templates/collection.html'
-         });
+        
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                controller: 'LandingCtrl as landing',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                controller: 'AlbumCtrl as album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                controller: 'CollectionCtrl as collection',
+                templateUrl: '/templates/collection.html'
+            });
     }
 
+    
 angular
     .module('blocJams',['ui.router'])
     .config(config);
